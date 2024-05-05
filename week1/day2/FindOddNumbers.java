8fb33279a7a57f01f423aecf35078f3e6faf0fa6
manojalagumalai <167837353+manojalagumalai@users.noreply.github.com>
2024-05-05 20:49:16 +0530

    Add files via upload

---
+++ week1/day2/FindOddNumbers.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, number=10;
+		for (i=0; i<=number; i++) {
+			if(i%2 !=0) {
+				System.out.println(i);
+			}			
+		}
+	}
+}
