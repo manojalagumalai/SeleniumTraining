ae2cba680e745e0a79531ab9572c5b2f5777f732
manojalagumalai <167837353+manojalagumalai@users.noreply.github.com>
2024-05-06 09:02:29 +0530

    Create AddSubClassCheckBoxButton.java

---
+++ week2/day3/Assignment2_Hybrid_Inheritance/AddSubClassCheckBoxButton.java
@@ -0,0 +1,11 @@
+package week2.day3.Assignment1;
+
+public class AddSubClassCheckBoxButton extends SubClassButton{
+	
+	public void clickCheckButton() {
+		
+	}
+	
+	AddSubClassCheckBoxButton obj = new AddSubClassCheckBoxButton();
+	
+}
